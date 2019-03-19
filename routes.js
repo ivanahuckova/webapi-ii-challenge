e06861e4015e224345c02b20bf1fb957d69a99d0
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-19 14:44:13 +0100

    Set up post routes

--- routes.js
+++ routes.js
@@ -29,4 +29,40 @@ routes.get('/api/posts/:id', async (req, res) => {
   }
 });
 
+// =========== POST ROUTES ========== \\
+
+/*When the client makes a `POST` request to `/api/posts`:
+
+- If the request body is missing the `title` or `contents` property:
+
+  - cancel the request.
+  - respond with HTTP status code `400` (Bad Request).
+  - return the following JSON response: `{ errorMessage: "Please provide title and contents for the post." }`.
+
+- If the information about the _post_ is valid:
+
+  - save the new _post_ the the database.
+  - return HTTP status code `201` (Created).
+  - return the newly created _post_.
+
+- If there's an error while saving the _post_:
+  - cancel the request.
+  - respond with HTTP status code `500` (Server Error).
+  - return the following JSON object: `{ error: "There was an error while saving the post to the database" }`.
+ */
+
+routes.post('/api/posts', async (req, res) => {
+  try {
+    const post = await db.insert(req.body);
+    if (post.id) {
+      const newPost = await db.findById(post.id);
+      res.status(201).json(newPost);
+    } else {
+      res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'There was an error while saving the post to the database' });
+  }
+});
+
 module.exports = routes;
