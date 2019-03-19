0ae8d9f692819a126047acfd2dbb39e7c77ded67
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-19 17:02:39 +0100

    Server.js change

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const postRoutes = require('./routes');
+const cors = require('cors');
+
 const server = express();
+
+server.use(express.json());
+server.use(cors());
 
 server.use('/api/posts', postRoutes);
 
