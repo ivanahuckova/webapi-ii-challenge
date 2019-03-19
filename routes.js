dadd22ae2db4e3400bccbec30b7939d2beea40df
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-19 15:05:20 +0100

    Set up put routes

--- routes.js
+++ routes.js
@@ -35,8 +35,8 @@ routes.post('/api/posts', async (req, res) => {
   try {
     const post = await db.insert(req.body);
     if (post.id) {
-      const newPost = await db.findById(post.id);
-      res.status(201).json(newPost);
+      const newPosts = await db.findById(post.id);
+      res.status(201).json(newPosts[0]);
     } else {
       res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' });
     }
@@ -50,13 +50,29 @@ routes.post('/api/posts', async (req, res) => {
 routes.delete('/api/posts/:id', async (req, res) => {
   try {
     const post = await db.remove(req.params.id);
-    if (post !== 0) {
+    if (post) {
       res.status(202).json({ message: `Post with id ${req.params.id} has been deleted` });
     } else {
       res.status(404).json({ message: 'The post with the specified ID does not exist.' });
     }
   } catch (error) {
     res.status(500).json({ error: 'The post could not be removed' });
+  }
+});
+
+// =========== PUT ROUTES ========== //
+
+routes.put('/api/posts/:id', async (req, res) => {
+  try {
+    const post = await db.update(req.params.id, req.body);
+    if (post) {
+      const changedPosts = await db.findById(req.params.id);
+      res.status(200).json(changedPosts[0]);
+    } else {
+      res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'The post information could not be modified.' });
   }
 });
 
