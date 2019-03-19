d948a52814e8fe77d0d4063b0edc64e8309b217a
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-19 14:52:53 +0100

    Set up delete routes

--- routes.js
+++ routes.js
@@ -5,7 +5,7 @@ const routes = express.Router();
 
 routes.use(express.json());
 
-// =========== GET ROUTES ========== \\
+// =========== GET ROUTES ========== //
 
 routes.get('/api/posts', async (req, res) => {
   try {
@@ -29,27 +29,7 @@ routes.get('/api/posts/:id', async (req, res) => {
   }
 });
 
-// =========== POST ROUTES ========== \\
-
-/*When the client makes a `POST` request to `/api/posts`:
-
-- If the request body is missing the `title` or `contents` property:
-
-  - cancel the request.
-  - respond with HTTP status code `400` (Bad Request).
-  - return the following JSON response: `{ errorMessage: "Please provide title and contents for the post." }`.
-
-- If the information about the _post_ is valid:
-
-  - save the new _post_ the the database.
-  - return HTTP status code `201` (Created).
-  - return the newly created _post_.
-
-- If there's an error while saving the _post_:
-  - cancel the request.
-  - respond with HTTP status code `500` (Server Error).
-  - return the following JSON object: `{ error: "There was an error while saving the post to the database" }`.
- */
+// =========== POST ROUTES ========== //
 
 routes.post('/api/posts', async (req, res) => {
   try {
@@ -62,6 +42,21 @@ routes.post('/api/posts', async (req, res) => {
     }
   } catch (error) {
     res.status(500).json({ error: 'There was an error while saving the post to the database' });
+  }
+});
+
+// =========== DELETE ROUTES ========== //
+
+routes.delete('/api/posts/:id', async (req, res) => {
+  try {
+    const post = await db.remove(req.params.id);
+    if (post !== 0) {
+      res.status(202).json({ message: `Post with id ${req.params.id} has been deleted` });
+    } else {
+      res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'The post could not be removed' });
   }
 });
 
