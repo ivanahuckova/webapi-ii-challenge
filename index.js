c820dc92b5915f83ec95a43714a1dac5029b2b02
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-21 13:47:11 +0100

    Set up index.js for deployment

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const server = require('./server');
+const express = require('express');
 
-server.listen(9000, () => {
-  console.log(`Server is running on port 9000`);
+const port = process.env.PORT || 8080;
+
+const pathToBuildFolder = path.join(__dirname, 'build');
+const pathToIndexHtml = path.join(pathToBuildFolder, 'index.html');
+
+server.use(express.static(pathToBuildFolder));
+server.get('/', (req, res) => {
+  res.sendFile(pathToIndexHtml);
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
